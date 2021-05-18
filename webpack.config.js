55b6e1320bbd4dc353b1d3e93409303452ab5680
Johan von Boer <johan.von.boer@umu.se>
2021-05-18 12:33:41 +0200

    build now includes R scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
   target: 'node',
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "scripts", to: "scripts" }
+      ],
+    }),
+  ]
 };
