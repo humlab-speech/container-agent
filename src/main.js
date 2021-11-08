d08e1dc928cb2e03b95ef1c6a51815f67a39e294
Johan von Boer <johan.von.boer@umu.se>
2021-11-08 12:00:12 +0100

    Added simulate command to easify debugging

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ const ApiResponse = require('./ApiResponse.class.js');
 const GitRepository = require('./GitRepository.class.js');
 const EmuDbManager = require('./EmuDbManager.class.js');
 const dotenv = require('dotenv');
+const fs = require('fs');
 
 if(typeof process.env.CONTAINER_AGENT_TEST == "undefined") {
     process.env.CONTAINER_AGENT_TEST = 'false';
@@ -64,6 +65,73 @@ async function copyProjectTemplateDirectory() {
     });
 }
 
+async function simulateProjectCreation(emudbMan) {
+    process.env['SIMULATION'] = "true";
+
+    //Check that env-var PROJECT_PATH is set, otherwise set it to something default.
+    if(!process.env['PROJECT_PATH']) {
+        process.env['PROJECT_PATH'] = "/home/project-setup";
+        fs.mkdirSync(process.env['PROJECT_PATH'], {
+            recursive: true
+        })
+    }
+    //Check that EMUDB_SESSIONS is set, otherwise set it to something default.
+    if(!process.env['EMUDB_SESSIONS']) {
+        let sessionsJson = `[{"id":"session-JPWUR30FXM9SMke8LGLQz","name":"Speaker_1","speakerGender":"Female","speakerAge":35,"files":[{"uploadComplete":true}]}]`;
+        let b = new Buffer.from(sessionsJson, "utf8");
+        process.env['EMUDB_SESSIONS'] = b.toString('base64');
+    }
+
+    if(!process.env['UPLOAD_PATH']) {
+        process.env['UPLOAD_PATH'] = "/home/uploads";
+    }
+
+    if(!process.env['ANNOT_LEVELS']) {
+        let annotLevelsJson = `[{"name":"Word","type":"ITEM"},{"name":"Phonetic","type":"SEGMENT"}]`;
+        let b = new Buffer.from(annotLevelsJson, "utf8");
+        process.env['ANNOT_LEVELS'] = b.toString('base64');
+    }
+
+    //emudb-create
+    console.log("create");
+    await emudbMan.create().then(ar => console.log(ar.toJSON())).catch(ar => console.log(ar.toJSON()));
+    //emudb-create-sessions
+    console.log("\ncreateSessions");
+    await emudbMan.createSessions().then(ar => console.log(ar.toJSON())).catch(ar => console.log(ar.toJSON()));
+    //emudb-create-bundlelist
+    console.log("\ncreateBundleList");
+    await emudbMan.createBundleList().then(ar => console.log(ar.toJSON())).catch(ar => console.log(ar.toJSON()));
+    //emudb-create-annotlevels
+    process.env['ANNOT_LEVEL_DEF_NAME'] = "Word";
+    process.env['ANNOT_LEVEL_DEF_TYPE'] = "ITEM";
+    console.log("\ncreateAnnotationLevels");
+    await emudbMan.createAnnotationLevels().then(ar => console.log(ar.toJSON())).catch(ar => console.log(ar.toJSON()));
+    process.env['ANNOT_LEVEL_DEF_NAME'] = "Phonetic";
+    process.env['ANNOT_LEVEL_DEF_TYPE'] = "SEGMENT";
+    console.log("\ncreateAnnotationLevels");
+    await emudbMan.createAnnotationLevels().then(ar => console.log(ar.toJSON())).catch(ar => console.log(ar.toJSON()));
+    //emudb-create-annotlevellinks
+    process.env['ANNOT_LEVEL_LINK_SUPER'] = "Word";
+    process.env['ANNOT_LEVEL_LINK_SUB'] = "Phonetic";
+    process.env['ANNOT_LEVEL_LINK_DEF_TYPE'] = "ONE_TO_MANY";
+    console.log("\ncreateAnnotationLevelLinks");
+    await emudbMan.createAnnotationLevelLinks().then(ar => console.log(ar.toJSON())).catch(ar => console.log(ar.toJSON()));
+    //emudb-setlevelcanvasesorder
+    console.log("\nsetLevelCanvasesOrder");
+    await emudbMan.setLevelCanvasesOrder().then(ar => console.log(ar.toJSON())).catch(ar => console.log(ar.toJSON()));
+    //emudb-add-default-perspectives
+    console.log("\naddDefaultPerspectives");
+    await emudbMan.addDefaultPerspectives().then(ar => console.log(ar.toJSON())).catch(ar => console.log(ar.toJSON()));
+    //emudb-track-definitions
+    console.log("\naddTrackDefinitions");
+    await emudbMan.addTrackDefinitions().then(ar => console.log(ar.toJSON())).catch(ar => console.log(ar.toJSON()));
+    //full-recursive-copy
+    //await fullRecursiveCopy(args[0], args[1]).then(ar => console.log(ar.toJSON())).catch(ar => console.log(ar.toJSON()));
+    //...and then a push to git
+
+    console.log("All done");
+}
+
 if(typeof process.argv[2] == 'undefined') {
     return console.log('No command supplied');
 }
@@ -101,11 +169,14 @@ else {
     }
 
     let emudbMan = null;
-    if(cmd.split("-")[0] == "emudb") {
+    if(cmd.split("-")[0] == "emudb" || cmd == "simulate") {
         emudbMan = new EmuDbManager(this);
     }
     
     switch(cmd) {
+        case "simulate":
+            simulateProjectCreation(emudbMan)
+            break;
         case "copy-docs":
             copyDocs().then(ar => console.log(ar.toJSON())).catch(ar => console.log(ar.toJSON()));
             break;
