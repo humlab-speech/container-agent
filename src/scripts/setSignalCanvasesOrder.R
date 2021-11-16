b14802f54cc2cc98ac9143a72829fa1b9951857a
Johan von Boer <johan.von.boer@umu.se>
2021-11-16 10:34:18 +0100

    fixed F0 begin shown in all perspectives

--- src/scripts/setSignalCanvasesOrder.R
+++ src/scripts/setSignalCanvasesOrder.R
@@ -5,8 +5,10 @@ library(base64enc)
 dbPath = file.path(Sys.getenv("PROJECT_PATH"), "Data", "VISP_emuDB")
 VISPDB = load_emuDB(dbPath)
 
-levelOrder = c("OSCI", "SPEC", "F0")
+levelOrder = c("OSCI", "SPEC")
 
 set_signalCanvasesOrder(VISPDB, perspectiveName = "default", order = levelOrder)
 set_signalCanvasesOrder(VISPDB, perspectiveName = "Formants", order = levelOrder)
+
+levelOrder <- c(levelOrder, c("F0"))
 set_signalCanvasesOrder(VISPDB, perspectiveName = "Formants+F0", order = levelOrder)
