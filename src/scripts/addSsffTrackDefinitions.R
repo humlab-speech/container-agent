6083fb70267f9aa161c537ce9a5068484008a561
Johan von Boer <johan.von.boer@umu.se>
2024-02-05 16:15:00 +0100

    (temporar) fix for issue #194

--- src/scripts/addSsffTrackDefinitions.R
+++ src/scripts/addSsffTrackDefinitions.R
@@ -3,5 +3,8 @@ library(reindeer)
 dbPath = file.path(Sys.getenv("PROJECT_PATH"), "Data", "VISP_emuDB")
 VISPDB = load_emuDB(dbPath)
 
-add_trackDefinition(VISPDB, name = "FORMANTS", onTheFlyFunctionName = "forest")
-add_trackDefinition(VISPDB, name = "F0", onTheFlyFunctionName = "ksvF0")
+#add_trackDefinition(VISPDB, name = "FORMANTS", onTheFlyFunctionName = "forest")
+#add_trackDefinition(VISPDB, name = "F0", onTheFlyFunctionName = "ksvF0")
+
+emuR::add_ssffTrackDefinition(VISPDB, name = "FORMANTS", onTheFlyFunctionName = "forest")
+emuR::add_ssffTrackDefinition(VISPDB, name = "F0", onTheFlyFunctionName = "ksvF0")
